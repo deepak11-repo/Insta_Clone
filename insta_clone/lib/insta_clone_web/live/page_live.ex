7572d4c8f0ffcb13bb25ac014110df1927b999e3
Deepak Naidu <deepaknaidu1122@gmail.com>
2024-05-08 12:10:51 +0530

    Second Commit

--- insta_clone/lib/insta_clone_web/live/page_live.ex
+++ insta_clone/lib/insta_clone_web/live/page_live.ex
@@ -1,8 +1,11 @@
 defmodule InstaCloneWeb.PageLive do
   use InstaCloneWeb, :live_view
+  alias InstaClone.Accounts
+  alias InstaClone.Accounts.User
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    changeset = Accounts.change_user_registration(%User{})
+    {:ok, socket  |> assign(changeset: changeset)}
   end
 end
