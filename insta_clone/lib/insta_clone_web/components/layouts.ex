f5c597ed36e0d0b31c71bc6a4b81a8f1a1685b04
Deepak Naidu <deepaknaidu1122@gmail.com>
2024-05-08 11:56:55 +0530

    First Commit

---
+++ insta_clone/lib/insta_clone_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule InstaCloneWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use InstaCloneWeb, :controller` and
+  `use InstaCloneWeb, :live_view`.
+  """
+  use InstaCloneWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ insta_clone/lib/insta_clone_web/live/page_live.ex
@@ -0,0 +1,8 @@
+defmodule InstaCloneWeb.PageLive do
+  use InstaCloneWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
